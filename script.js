e88e4b46e56e044750be62d6b9e20f2d2c6836d9
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-11 10:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-let cost=0;
-arr.sort((a, b) => b - a);
+let cost = 0;
+let copyArr = [...arr]; // Create a copy of the original array
 
-while(arr.length>0)
-	{
-		let temp=[];
-		arr.sort((a, b) => b - a);
-		let a= arr.length>0 ? arr.pop() : 0;
-		let b= arr.length>0 ? arr.pop() : 0;
-		cost+= a+b;
-		if(a+b>0)
-		{
-			arr.push(a+b);
-		}
-	}
-return  cost;
+copyArr.sort((a, b) => b - a); // Sort the copy in descending order
+
+while (copyArr.length > 0) {
+    copyArr.sort((a, b) => b - a); // Sort the copy again in case it was modified
+    let a = copyArr.length > 0 ? copyArr.pop() : 0; // Remove the last element from the copy
+    let b = copyArr.length > 0 ? copyArr.pop() : 0; // Remove the new last element from the copy
+    cost += a + b; // Update the cost
+    if (a + b > 0) {
+        copyArr.push(a + b); // Add the sum of the removed elements back to the copy
+    }
+}
+
+return cost; // Return the final cost
+
 }
 
 module.exports=mincost;
