1037ca1cad3176f7974633ef0b368c60b936dc6b
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-11 10:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+let cost=0;
+arr.sort((a, b) => b - a);
+
+while(arr.length>0)
+	{
+		let temp=[];
+		while(arr.length>0)
+			{
+				let a= arr.length>0 ? arr.pop() : 0;
+				let b= arr.length>0 ? arr.pop() : 0;
+				cost+= a+b;
+				if(a+b>0)
+				{
+					temp.push(a+b);
+				}
+			}
+		arr=temp;
+	}
+return  cost;
 }
 
 module.exports=mincost;
